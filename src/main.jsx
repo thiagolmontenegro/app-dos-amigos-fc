75723d2a19bbf8bd6e25276d22cba01d99b28827
thiagolmontenegro <thiagolmontenegro@gmail.com>
2026-04-28 16:20:13 -0300

    UpdateFinal

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from "react"; import {createRoot} from "react-dom/client"; import "./index.css"; function App(){return <h1 className="text-3xl text-green-400 p-6">APP OK</h1>} createRoot(document.getElementById("root")).render(<App/>)
